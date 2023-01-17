d7f487976719bc75d0898dd8ca733b3a2d9aa3bb
naamhee <kimnamhee391@gmail.com>
2023-01-17 23:41:22 +0900

    테스트커밋

--- src/main/java/com/crosscert/firewall/FirewallApplication.java
+++ src/main/java/com/crosscert/firewall/FirewallApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirewallApplication {
 
+	//테스트
 	public static void main(String[] args) {
 		SpringApplication.run(FirewallApplication.class, args);
 	}
