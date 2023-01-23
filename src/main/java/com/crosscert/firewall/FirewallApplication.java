f2da4baace3f3a4c97039b10f105cb0d5125e7fc
ShinYeahChan <syeahc@gmail.com>
2023-01-23 15:55:11 +0900

    Push Test 2

--- src/main/java/com/crosscert/firewall/FirewallApplication.java
+++ src/main/java/com/crosscert/firewall/FirewallApplication.java
@@ -1,5 +1,6 @@
 package com.crosscert.firewall;
 
+import com.crosscert.firewall.testgit.PushTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class FirewallApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirewallApplication.class, args);
+		PushTest.pushTest();
 	}
 
 }
--- src/main/java/com/crosscert/firewall/testgit/PushTest.java
+++ src/main/java/com/crosscert/firewall/testgit/PushTest.java
@@ -3,5 +3,6 @@ package com.crosscert.firewall.testgit;
 public class PushTest {
     public static void pushTest(){
         System.out.println("Push Test");
+        System.out.println("Push Test2");
     }
 }
