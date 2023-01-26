9a159e162c55922ab903a04294297e42d8df9889
ShinYeahChan <syeahc@gmail.com>
2023-01-26 23:58:39 +0900

    스프링시큐리티 기본 설정

--- src/main/java/com/crosscert/firewall/security/WebSecurityConfig.java
+++ src/main/java/com/crosscert/firewall/security/WebSecurityConfig.java
@@ -1,18 +1,38 @@
 package com.crosscert.firewall.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//@RequiredArgsConstructor
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {     //deprecated
+public class WebSecurityConfig {
 
     //스프링 시큐리티 설정 진행중
-    //deprecated
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/user/**").authenticated()
+                    .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .and().build();
+    }
+
+    
+
 }
