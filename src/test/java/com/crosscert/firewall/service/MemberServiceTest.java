6592baf3f6d738054bd9cdf3c5ce23350fe66fd3
Choi ByungJae <wlvvwhs@naver.com>
2023-02-05 23:42:23 +0900

    member edit 추가

--- src/test/java/com/crosscert/firewall/service/MemberServiceTest.java
+++ src/test/java/com/crosscert/firewall/service/MemberServiceTest.java
@@ -16,7 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @SpringBootTest
 @Transactional
