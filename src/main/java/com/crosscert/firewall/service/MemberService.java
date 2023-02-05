3c2950d41d2b775169a30b296eb67fee094fa322
ShinYeahChan <syeahc@gmail.com>
2023-02-05 23:53:22 +0900

    회원가입 기능 개발 - 중복 회원 확인 + 유저 정보 저장, 회원가입 기본 뷰단(CSS는 추후 적용)

--- src/main/java/com/crosscert/firewall/service/MemberService.java
+++ src/main/java/com/crosscert/firewall/service/MemberService.java
@@ -1,9 +1,11 @@
 package com.crosscert.firewall.service;
 
 import com.crosscert.firewall.dto.MemberDTO;
+import com.crosscert.firewall.entity.Member;
 import com.crosscert.firewall.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,6 +25,29 @@ public class MemberService {
                         m.getDevIp().getAddress().getAddress(),
                         m.getNetIp().getAddress().getAddress()))
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public String signup(MemberDTO.Request.Create memberDTO) {
+
+        //중복 회원 검증
+        List<Member> findMembers = memberRepository.findByEmail(memberDTO.getEmail());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+
+        Member member = Member.builder()
+                .name(memberDTO.getName())
+                .email(memberDTO.getEmail())
+                .password(memberDTO.getPassword())
+                .role(memberDTO.getRole()).build();
+
+        memberRepository.save(member);
+
+        //TODO : IP 정보 저장
+
+        return member.getEmail();
+
     }
 }
 
