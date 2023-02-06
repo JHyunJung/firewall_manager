a2996bdf0adb1ab748809ae19dabc317a8606272
ShinYeahChan <syeahc@gmail.com>
2023-02-06 19:31:58 +0900

    SignUpDTO삭제, MemberDTO.Request.Create 사용

--- src/main/java/com/crosscert/firewall/controller/LoginAPIController.java
+++ src/main/java/com/crosscert/firewall/controller/LoginAPIController.java
@@ -1,8 +1,8 @@
 package com.crosscert.firewall.controller;
 
 import com.crosscert.firewall.dto.MemberDTO;
-import com.crosscert.firewall.dto.SignUpDTO;
 import com.crosscert.firewall.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,29 +10,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Log4j2
 @Controller
+@RequiredArgsConstructor
 public class LoginAPIController {
 
     private final MemberService memberService;
 
-    public LoginAPIController(MemberService memberService) {
-        this.memberService = memberService;
-    }
-
     //회원가입 진행   //TODO: MemberApiController에서 진행할지 결정 필요.
     @PostMapping("/signup")
     @ResponseBody
-    public String signup(SignUpDTO signUpDTO){  //비밀번호 암호화 진행
+    public String signup(MemberDTO.Request.Create memberDTO){
         log.info("{}.signup",this.getClass());
-        log.info("signUpDTO : "+signUpDTO);
 
-        MemberDTO.Request.Create memberDTO = new MemberDTO.Request.Create(
-                signUpDTO.getName(),
-                signUpDTO.getEmail(),
-                signUpDTO.getPassword(),
-                signUpDTO.getRole(),
-                signUpDTO.getDevIp(),
-                signUpDTO.getNetIp()
-        );
+        //:TODO 비밀번호 암호화
+
         log.info("memberDTO : "+memberDTO);
 
         String newMemberEmail = memberService.signup(memberDTO);
