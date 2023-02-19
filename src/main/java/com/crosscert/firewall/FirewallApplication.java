8a578d550d7a1438f1cf69627fb8a484dd7c98c9
naamhee <kimnamhee391@gmail.com>
2023-02-20 01:08:11 +0900

    문구삭제

--- src/main/java/com/crosscert/firewall/FirewallApplication.java
+++ src/main/java/com/crosscert/firewall/FirewallApplication.java
@@ -7,8 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class FirewallApplication {
-
-	//테스트
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FirewallApplication.class, args);
 	}
