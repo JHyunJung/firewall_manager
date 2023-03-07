ce64246a03bb703427b05d9eb0b194a9e8fdded8
ShinYeahChan <syeahc@gmail.com>
2023-03-07 23:46:57 +0900

    디자인 템플릿 적용, 타임리프 레이아웃 처리

--- src/main/java/com/crosscert/firewall/controller/LoginController.java
+++ src/main/java/com/crosscert/firewall/controller/LoginController.java
@@ -1,14 +1,11 @@
 package com.crosscert.firewall.controller;
 
-import com.crosscert.firewall.annotation.LogTrace;
 import com.crosscert.firewall.dto.MemberDTO;
 import com.crosscert.firewall.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
