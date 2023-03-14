16989155b53265aa32750a6f7cb40d02fb7a5716
ShinYeahChan <syeahc@gmail.com>
2023-03-14 20:14:52 +0900

    IP중복검사 테스트코드

--- src/test/java/com/crosscert/firewall/service/IPServiceTest.java
+++ src/test/java/com/crosscert/firewall/service/IPServiceTest.java
@@ -1,9 +1,12 @@
 package com.crosscert.firewall.service;
 
+import com.crosscert.firewall.dto.MemberDTO;
 import com.crosscert.firewall.entity.IP;
 import com.crosscert.firewall.entity.IpAddress;
+import com.crosscert.firewall.entity.Role;
 import com.crosscert.firewall.repository.IPRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 @SpringBootTest
@@ -23,6 +28,11 @@ class IPServiceTest {
 
     @Autowired
     IPService ipService;
+
+    @BeforeEach
+    void clean(){
+        ipRepository.deleteAll();
+    }
 
     @Test
     @DisplayName("findByAddress Exception")
@@ -40,5 +50,26 @@ class IPServiceTest {
             ipService.findById(2L);
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("해당 IP가 존재하지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("isPresentIp Test")
+    void isPresentIp() {
+        // Given
+        IP ip = IP.builder()
+                .domain("testdomain.com")
+                .description("테스트 IP")
+                .address(new IpAddress("172.77.0.1"))
+                .build();
+        ipService.save(ip);
+
+
+        // When
+        String ip1 =  "172.77.0.3";
+        String ip2 = "172.77.0.1";   //IP 중복
+
+        // Then
+        assertFalse(ipService.isPresentIp(new IpAddress(ip1)));
+        assertTrue(ipService.isPresentIp(new IpAddress(ip2)));  //IP 중복시 true
     }
 }
