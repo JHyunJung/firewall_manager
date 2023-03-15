d4bb1a362c5fc7225ee085d6212fd052e0b3e527
JhyunJung <jhyun.rigel@gmail.com>
2023-03-15 20:25:16 +0900

    임시

---
+++ src/main/java/com/crosscert/firewall/dto/FireWallDTO.java
@@ -0,0 +1,65 @@
+package com.crosscert.firewall.dto;
+
+import com.crosscert.firewall.entity.Ip;
+import com.crosscert.firewall.entity.Role;
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.List;
+import java.util.Map;
+
+public enum FireWallDTO {;
+    private interface Id { Long getId();}
+    private interface Start { String getStart(); }
+    private interface Destination { String getStart(); }
+    private interface Port { int getPort(); }
+    private interface EndDate { String getEndDate(); }
+    private interface Ended { boolean isEnded(); }
+    private interface Member { String getMember(); }
+
+
+    public enum Request{;
+
+        @Value
+        public static class Create implements Start, Destination, Port, EndDate, Member{
+            String start;
+            String destination;
+            int port;
+            String endDate;
+            String member;
+        }
+    }
+
+    public enum Response{;
+
+        @Builder
+        @Value
+        public static class Create implements Id{
+            Long id;
+        }
+
+        @Builder
+        @Value
+        public static class Public implements Id, Start, Destination, Port, EndDate, Ended{
+            Long id;
+            String start;
+            String destination;
+            int port;
+            String endDate;
+            boolean ended;
+        }
+
+//        public static class ListPage<T> implements Id, Start, Destination, Port, EndDate, Ended{
+//
+//            private Map<String, Object> pageInfo;
+//            private List<T> content;
+//
+//            Long id;
+//            String start;
+//            String destination;
+//            String port;
+//            String endDate;
+//            boolean ended;
+//        }
+    }
+}
