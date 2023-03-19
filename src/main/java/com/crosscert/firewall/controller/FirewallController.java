181844359168c5370a6f3e19c253617f21748634
JhyunJung <jhyun.rigel@gmail.com>
2023-03-19 18:59:24 +0900

    merge

---
+++ src/main/java/com/crosscert/firewall/controller/FirewallController.java
@@ -0,0 +1,28 @@
+package com.crosscert.firewall.controller;
+
+
+import com.crosscert.firewall.service.FireWallService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class FireWallController {
+
+
+    private final FireWallService fireWallService;
+
+
+    @GetMapping("/firewalls")
+    public String fireWalls(){
+
+
+//        Page<FireWall> fireWallPage = fireWallService.findAll(0, 10);
+//        PageInfo pageInfo = new PageInfo(0, 10, (int) fireWallPage.getTotalElements(), fireWallPage.getTotalElements());
+//
+//        model.addAttribute("members", members);
+        return "firewalls";
+    }
+
+}
---
+++ src/test/java/com/crosscert/firewall/service/FireWallServiceTest.java
@@ -0,0 +1,52 @@
+package com.crosscert.firewall.service;
+
+import com.crosscert.firewall.config.DatabaseCleanup;
+import com.crosscert.firewall.entity.FireWall;
+import com.crosscert.firewall.entity.IP;
+import com.crosscert.firewall.entity.IpAddress;
+import com.crosscert.firewall.entity.search.SearchFirewall;
+import com.crosscert.firewall.repository.FireWallRepository;
+import com.crosscert.firewall.repository.FirewallRepository;
+import javafx.beans.binding.ObjectExpression;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class FireWallServiceTest {
+
+    @Autowired
+    FireWallRepository fireWallRepository;
+
+    @Autowired
+    FireWallService fireWallService;
+
+    @Autowired
+    DatabaseCleanup databaseCleanup;
+
+    @BeforeEach
+    void clean(){
+        databaseCleanup.execute();
+    }
+
+    @Test
+    @DisplayName("findById Exception")
+    void findByIdException() {
+        assertThatThrownBy(() -> {
+            fireWallService.findById(1L);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("해당 방확벽이 존재하지 않습니다.");
+    }
+
+
+}
